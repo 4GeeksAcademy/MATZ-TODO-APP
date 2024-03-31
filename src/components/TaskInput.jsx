c0d8c8889c12c985fba99ee82798379e347fcde8
Matz72 <156270489+Matz72@users.noreply.github.com>
2024-03-31 16:35:38 +0000

    save changes

--- src/components/TaskInput.jsx
+++
@@ -1,9 +0,0 @@
-export function TaskInput({ task, onTaskChange, onAddTask }) {
-    return (
-      <div>
-        <input value={task} onChange={(ev) => onTaskChange(ev.target.value)} />
-        <button onClick={onAddTask}>Add</button>
-      </div>
-    );
-  }
-  
