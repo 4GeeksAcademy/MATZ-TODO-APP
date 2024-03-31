2df887f10395807104700b8b318cd9f6851224c3
Matz72 <156270489+Matz72@users.noreply.github.com>
2024-03-31 16:56:09 +0000

    save changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,13 @@
-// index.jsx
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './Home';
-import './index.css';
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// include your styles into the webpack bundle
+import "../styles/index.css";
+
+//import your own components
+import Home from "./component/home.jsx";
+
+//render your react application
+ReactDOM.render(<Home />, document.querySelector("#app"));
+
