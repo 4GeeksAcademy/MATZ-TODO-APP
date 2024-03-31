01680b7c331397477203d840da0cf9eed5520ad3
Matz72 <156270489+Matz72@users.noreply.github.com>
2024-03-31 16:19:02 +0000

    save changes

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoApp from './TodoApp'; // Correct the import path accordingly
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
